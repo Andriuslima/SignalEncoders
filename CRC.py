d6fd17868d31a4effd1f52cf6178eb498208a3e6
Andrius Lima <andrius.lima@neogrid.com>
2019-04-23 15:57:51 -0300

    CRC decoder done

--- CRC.py
+++ CRC.py
@@ -1,64 +1,83 @@
 import sys
-import utils
+from operator import xor
 
 ENCODER = '-e'
 DECODER = '-d'
+DEBUG = False
 
 
-class Encoder:
+def string_to_ascii_dict(string):
+    ans = {}
+    for char in string:
+        ans[char] = bin(ord(char))[2:].rjust(7, "0")
+    return ans
+
+
+def xor_in_list(x, y):
+    ans = ""
+    if len(x) != len(y):
+        raise ValueError(f'Not the same length: {x} and {y}\n')
+    for i in range(len(x)):
+        ans += str(int(xor(int(x[i]), int(y[i]))))
+    return ans
+
+
+def xor_operation(code, polynomial):
+    code = code.ljust(len(code)+len(polynomial)-1, "0")
+    x = code[0:len(polynomial)]
+    y = polynomial if int(x[0]) != 0 else "0" * len(polynomial)
+    previews = xor_in_list(x, y)
+
+    for i in range(len(code)-len(polynomial)):
+
+        x = previews[1:] + code[i+len(polynomial)]
+        y = polynomial if int(x[0]) != 0 else "0" * len(polynomial)
+
+        previews = xor_in_list(x, y)
+    return previews[1:]
+
+
+class CRC:
     def __init__(self, msg, polynomial):
         self.msg = msg
         self.polynomial = polynomial
-        self.chars = utils.string_to_ascii_dict(msg)
+        self.chars = string_to_ascii_dict(msg)
         self.char_encoded = dict()
 
     def encode(self):
         for char, code in self.chars.items():
-            #print(f'Encode {code}')
-            self.char_encoded[char] = self.xor_operation(code)
-            #print(f'Character {char} encoded to {result}\n')
+            self.char_encoded[char] = xor_operation(code, self.polynomial)
 
         ans = ""
         for char in self.msg:
             ans += hex(int(self.chars[char], 2))[2:] + hex(int(self.char_encoded[char], 2))[2:]
-            #print(f'{char} = {self.char_encoded[char]}\n')
-        print(ans)
-
-
-
-    def xor_operation(self, code):
-        code = code.ljust(len(code)+len(self.polynomial)-1, "0")
-        #print(f'Code transformed to {code}\n')
-        x = code[0:len(self.polynomial)]
-        y = self.polynomial if int(x[0]) != 0 else "0" * len(self.polynomial)
-        previews = utils.xor_in_list(x, y)
-
-        for i in range(len(code)-len(self.polynomial)):
-
-            x = previews[1:] + code[i+len(self.polynomial)]
-            y = self.polynomial if int(x[0]) != 0 else "0" * len(self.polynomial)
-
-            previews = utils.xor_in_list(x, y)
-        return previews[1:]
-
-
-class Decoder:
-    def __init__(self, msg, polynomial):
-        self.msg = msg
-        self.polynomial = polynomial
+        print(ans, file=sys.stdout)
 
     def decode(self):
-        pass
+        ans = ""
+        if len(self.msg) % 3:
+            raise ValueError(f'Not the correct length: {self.msg}\n')
+        for i in range(0, len(self.msg)-1, 3):
+            char = bin(int(self.msg[i:i+2], 16))[2:]
+            code = bin(int( self.msg[i+2], 16))[2:].zfill(4)
+
+            result = xor_operation(char+code, self.polynomial)
+            if DEBUG: print(f'char: {char} | poly: {code} | result: {result}')
+            if int(result) == 0:
+                ans += chr(int(char, 2))
+            else:
+                ans += chr(95)
+        print(ans)
 
 
 operation = sys.argv[1]
 msg = sys.argv[2]
 polynomial = sys.argv[3]
 
+crc = CRC(msg, polynomial)
 
 if operation == ENCODER:
-    enc = Encoder(msg, polynomial)
-    enc.encode()
+    crc.encode()
+
 elif operation == DECODER:
-    dec = Decoder(msg, polynomial)
-    dec.decode()
+    crc.decode()
